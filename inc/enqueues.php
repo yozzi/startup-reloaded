c6f757da673b82f97b2c21eaffa91680e4ed1f4c
yozzi <yozz.online@gmail.com>
2015-08-11 16:28:24 -0400

    Minor

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -20,7 +20,7 @@ function startup_reloaded_scripts() {
     
     wp_enqueue_style( 'startup-reloaded-ytplayer', get_template_directory_uri() . '/lib/jquery.mb.YTPlayer/dist/css/jquery.mb.YTPlayer.min.css' );
     
-        wp_enqueue_style( 'startup-reloaded-magnific-popup', get_template_directory_uri() . '/lib/Magnific-Popup/dist/magnific-popup.css' );
+    wp_enqueue_style( 'startup-reloaded-magnific-popup', get_template_directory_uri() . '/lib/Magnific-Popup/dist/magnific-popup.css' );
 	
     wp_enqueue_style( 'startup-reloaded-style', get_stylesheet_uri() );
     
