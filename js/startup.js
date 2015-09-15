b0982c9c7e82ed6ce0c85468f73f4c3962e4216f
yozz <yozz.online@gmail.com>
2015-09-15 13:13:56 -0400

    Navbar modal jump dirty fix

--- js/startup.js
+++ js/startup.js
@@ -119,3 +119,18 @@ function scrollToTop() {
 	offsetTop = offset.top;
 	$('html, body').animate({scrollTop: offsetTop}, 200, 'linear');
 }
+
+// Navbar modal jump dirty fix
+var fixedCls = '.navbar-fixed-top,.navbar-fixed-bottom';
+    var oldSSB = $.fn.modal.Constructor.prototype.setScrollbar;
+    $.fn.modal.Constructor.prototype.setScrollbar = function () {
+        oldSSB.apply(this);
+        if (this.bodyIsOverflowing && this.scrollbarWidth)
+            $(fixedCls).css('padding-right', this.scrollbarWidth);
+    }
+
+    var oldRSB = $.fn.modal.Constructor.prototype.resetScrollbar;
+    $.fn.modal.Constructor.prototype.resetScrollbar = function () {
+        oldRSB.apply(this);
+        $(fixedCls).css('padding-right', '');
+    }
