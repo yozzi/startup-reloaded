870ab02d668182993a2bff2f7756f6df962a2143
yozz <Yann@Manava.local>
2015-09-15 19:04:00 -0400

    Dev

--- inc/navbar-primary.php
+++ inc/navbar-primary.php
@@ -17,17 +17,19 @@
 
                     
                     
-    <nav id="site-navigation" class="navbar navbar-default <?php if( $boxed ){ echo 'navbar-boxed '; }  echo $navbar_position; if ($navbar_inverse) { echo 'navbar-inverse'; }; ?> <?php if ($navbar_transparent  && ( $navbar_position == 'navbar-fixed-top' ) && is_front_page()) { echo 'navbar-transparent'; }; ?>" role="navigation">
+    <nav id="site-navigation" class="navbar navbar-default <?php if( $boxed ){ echo 'navbar-boxed '; }  echo $navbar_position; if ($navbar_inverse) { echo ' navbar-inverse'; }; ?> <?php if ($navbar_transparent  && ( $navbar_position == 'navbar-fixed-top' ) && is_front_page()) { echo 'navbar-transparent'; }; ?>" role="navigation">
         <!– Brand and toggle get grouped for better mobile display –>
         <div class="container">
             <div class="navbar-header <?php echo $navbar_logo_position; ?>-sm">
-                <?php if ($responsive == 1) { ?>
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-sur-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar top-bar"></span>
-                        <span class="icon-bar middle-bar"></span>
-                        <span class="icon-bar bottom-bar"></span>
-                    </button>
+                <?php if (wp_nav_menu( array( 'theme_location' => 'navbar-primary', 'echo' => false )) !== false) { ?>
+                    <?php if ($responsive == 1) { ?>
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-sur-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar top-bar"></span>
+                            <span class="icon-bar middle-bar"></span>
+                            <span class="icon-bar bottom-bar"></span>
+                        </button>
+                    <?php } ?>
                 <?php } ?>
                 <?php if ( $fullscreen_panel_on && $fullscreen_panel_hamburger ){ ?>
                     <button id="fullscreen-hamburger" type="button" class="navbar-toggle" data-toggle="modal" data-target="#fullscreen-panel">
@@ -42,11 +44,13 @@
                 </a>
                 
             </div>
+            <?php if ( has_nav_menu( 'navbar-primary' ) ) { ?>
             <!– Collect the nav links, forms, and other content for toggling –>
             <div class="<?php if ($responsive == 1) { ?>collapse navbar-collapse navbar-sur-collapse <?php } ?><?php echo $navbar_menu_position; ?>-sm">
                 <?php wp_nav_menu(array( 'menu'=> 'navbar-primary', 'theme_location' => 'navbar-primary', 'depth' => 2, 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => 'wp_page_menu', 'walker' => new wp_bootstrap_navwalker()) ); ?>
             </div>
             <!– /.navbar-collapse –>
+            <?php } ?>
         </div>
         <!– /.container –>
     </nav>
