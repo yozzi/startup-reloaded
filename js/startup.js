ddfd19c592720c71f7755045d3177a94d244ff58
yozz <yozz.online@gmail.com>
2015-09-22 16:11:16 -0400

    Beautify

--- js/startup.js
+++ js/startup.js
@@ -102,11 +102,11 @@ $(document).ready(function () {
 });
 
 // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
-  $('.dropdown').on('show.bs.dropdown', function(e){
+$('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).fadeIn();
-  });
+});
 
-  // ADD SLIDEUP ANIMATION TO DROPDOWN //
-  $('.dropdown').on('hide.bs.dropdown', function(e){
+// ADD SLIDEUP ANIMATION TO DROPDOWN //
+$('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).fadeOut();
-  });
+});
