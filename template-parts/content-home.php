3960cb6dd0172d85f6d48a8e8f5933ef2cd618a3
yozz <yozz.online@gmail.com>
2015-10-07 11:32:20 -0400

    Plug to Home plugin

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -3,21 +3,29 @@
  * @package StartUp Reloaded
  */
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'startup-reloaded' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
 
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'startup-reloaded' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+<?php $hasposts = get_posts('post_type=home');
+    if ( is_plugin_active('startup-cpt-home/startup-cpt-home.php') && !empty ( $hasposts ) ) { ?>
+
+    coucou
+
+<?php } else { ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="entry-content">
+            <?php
+                /* translators: %s: Name of current post */
+                the_content( sprintf(
+                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'startup-reloaded' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                ) );
+            ?>
+
+            <?php
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'startup-reloaded' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
+    </article><!-- #post-## -->
+<?php } ?>
