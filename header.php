5ad59ba9c833d7aa317a7e0a0c480f659b804a8c
yozz <yozz.online@gmail.com>
2015-10-30 11:59:01 -0400

    À corriger

--- header.php
+++ header.php
@@ -348,7 +348,8 @@
             <a class="skip-link screen-reader-text" href="#content">
                 <?php esc_html_e( 'Skip to content', 'startup-reloaded' ); ?>
             </a>
-            <?php if( ($navbar_on && $navbar_position != 'navbar-fixed-slider') || ( $navbar_on && !is_front_page())){
+            <?php // Corriger ici pour le Template Page Slider 
+            if( ($navbar_on && $navbar_position != 'navbar-fixed-slider') || ( $navbar_on && !is_front_page())){
                     if (!is_page_template( 'page-slider.php' )) {
                         require get_template_directory() . '/inc/navbar-primary.php';
                     }
