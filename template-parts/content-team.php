639cae8178d8d2d7ed627eed083e50109ad9f399
yozz <yozzi@users.noreply.github.com>
2015-11-09 14:54:02 -0500

    Update content-team.php

--- template-parts/content-team.php
+++ template-parts/content-team.php
@@ -1,6 +1,10 @@
 <?php
 $slider = of_get_option( 'team-slider' );
-$order = of_get_option( 'team-order' );
+if ( $atts['order'] == 'shuffle' ) {
+    $order = 'rand';
+} else {
+    $order = of_get_option( 'team-order' );
+}
 $number = of_get_option( 'team-number' );
 if ( $number ) { $max = $number; } else {$max = -1;};
 if ( $atts['cat'] ) {
