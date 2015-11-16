1d4677730b3db33fbfd2c697d7ccbad5a6f1c396
yozz <yozz.online@gmail.com>
2015-11-16 15:26:09 -0500

    Dev

---
+++ template-parts/content-timeline.php
@@ -0,0 +1,31 @@
+<?php
+$args=array( 'post_type'=>'timeline', 'orderby' => 'menu_order','order' => 'ASC', 'numberposts' => -1 );
+$timelines = get_posts( $args );
+$total_timelines = count($timelines);
+?>
+<section id="timeline"<?php if ( $atts['bg'] ) { ?> style="background:<?php echo $atts['bg'] ?>"<?php } ?>>
+    <?php if (is_front_page()) { ?><div class="container"><?php } ?>
+    <section id="cd-timeline" class="cd-container">
+        <div class="row">
+            <?php foreach ($timelines as $key=> $timeline) {
+                $icon    = get_post_meta($timeline->ID, '_startup_reloaded_timeline_icon', true );
+                $color    = get_post_meta($timeline->ID, '_startup_reloaded_timeline_color', true );
+                $date    = get_post_meta($timeline->ID, '_startup_reloaded_timeline_date', true );
+            ?>
+                <div class="cd-timeline-block">
+                    <div class="cd-timeline-img" style="background:<?php echo $color ?>">
+                        <i class="fa fa-<?php echo $icon ?>"></i>
+                    </div>
+
+                    <div class="cd-timeline-content">
+                        <h2><?php echo $timeline->post_title ?></h2>
+                        <p><?php echo $timeline->content ?></p>
+                        <a href="#0" class="cd-read-more"><?php _e( 'Read more', 'startup-reloaded' ) ?></a>
+                        <span class="cd-date"><?php echo $date ?></span>
+                    </div>
+                </div>
+            <?php } // endforeach ?>
+        </div>
+    </section>
+    <?php if (is_front_page()) { ?></div><?php } ?>
+</section>
