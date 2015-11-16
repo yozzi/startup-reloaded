522ea5976dbf3aafe5c051b0f198f74a3248a46c
yozz <yozz.online@gmail.com>
2015-11-16 14:08:11 -0500

    Timeline select2

--- js/startup-admin.js
+++ js/startup-admin.js
@@ -16,4 +16,5 @@ jQuery( document ).ready(function() {
     jQuery('#_startup_reloaded_pricing_icon_2').select2();
     jQuery('#_startup_reloaded_pricing_icon_3').select2();
     jQuery('#_startup_reloaded_pricing_icon_4').select2();
+    jQuery('#_startup_reloaded_timeline_icon').select2();
 });
