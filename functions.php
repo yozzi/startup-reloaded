e7239cb19a69cc2cd455c51c359e7a27cc641601
yozz <yozz.online@gmail.com>
2015-11-26 16:34:03 -0500

    Responsive images in post content

--- functions.php
+++ functions.php
@@ -288,3 +288,23 @@ function startup_reloaded_editor_styles() {
 }
 
 add_action( 'admin_init', 'startup_reloaded_editor_styles' );
+
+
+// Make every post image responsive
+function startup_reloaded_content_responsive_class($content){
+
+        $content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
+        $document = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $document->loadHTML(utf8_decode($content));
+
+        $imgs = $document->getElementsByTagName('img');
+        foreach ($imgs as $img) {           
+           $img->setAttribute('class','img-responsive');
+        }
+
+        $html = $document->saveHTML();
+        return $html;   
+}
+
+add_filter('the_content', 'startup_reloaded_content_responsive_class');
