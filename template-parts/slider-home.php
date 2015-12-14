0a757209dce4a7d0242b4b53a0b140a651d1d98d
yozz <Yann@Manava.local>
2015-12-14 14:02:01 -0500

    Revert "Revert "Revert "Shortcake"""
    This reverts commit 16eb8d04277f4cbdca1fc4105f99103d8395d2ff.

--- template-parts/slider-home.php
+++ template-parts/slider-home.php
@@ -27,22 +27,22 @@
         <div class="carousel-inner" role="listbox">
             <?php
                 foreach ($sliders as $key=> $slider) {
-                    $position            = get_post_meta( $slider->ID, '_startup_cpt_slider_position', true );
-                    $effect              = get_post_meta( $slider->ID, '_startup_cpt_slider_effect', true );
-                    $background_color    = get_post_meta( $slider->ID, '_startup_cpt_slider_background_color', true );
-                    $background_position = get_post_meta( $slider->ID, '_startup_cpt_slider_background_position', true );
-                    $background_video    = get_post_meta( $slider->ID, '_startup_cpt_slider_background_video', true );
-                    $boxed               = get_post_meta( $slider->ID, '_startup_cpt_slider_boxed', true );
-                    $button_text         = get_post_meta( $slider->ID, '_startup_cpt_slider_button_text', true );
-                    $button_url          = get_post_meta( $slider->ID, '_startup_cpt_slider_button_url', true );
-                    $blank               = get_post_meta( $slider->ID, '_startup_cpt_slider_blank', true );
+                    $position            = get_post_meta( $slider->ID, '_startup_reloaded_slider_position', true );
+                    $effect              = get_post_meta( $slider->ID, '_startup_reloaded_slider_effect', true );
+                    $background_color    = get_post_meta( $slider->ID, '_startup_reloaded_slider_background_color', true );
+                    $background_position = get_post_meta( $slider->ID, '_startup_reloaded_slider_background_position', true );
+                    $background_video    = get_post_meta( $slider->ID, '_startup_reloaded_slider_background_video', true );
+                    $boxed               = get_post_meta( $slider->ID, '_startup_reloaded_slider_boxed', true );
+                    $button_text         = get_post_meta( $slider->ID, '_startup_reloaded_slider_button_text', true );
+                    $button_url          = get_post_meta( $slider->ID, '_startup_reloaded_slider_button_url', true );
+                    $blank               = get_post_meta( $slider->ID, '_startup_reloaded_slider_blank', true );
                     $bg_image_url        = wp_get_attachment_url( get_post_thumbnail_id( $slider->ID ) );
-                    $title_animation     = get_post_meta( $slider->ID, '_startup_cpt_slider_title_animation', true );
-                    $title_delay         = get_post_meta( $slider->ID, '_startup_cpt_slider_title_delay', true );
-                    $content_animation   = get_post_meta( $slider->ID, '_startup_cpt_slider_content_animation', true );
-                    $content_delay       = get_post_meta( $slider->ID, '_startup_cpt_slider_content_delay', true );
-                    $button_animation    = get_post_meta( $slider->ID, '_startup_cpt_slider_button_animation', true );
-                    $button_delay        = get_post_meta( $slider->ID, '_startup_cpt_slider_button_delay', true );
+                    $title_animation     = get_post_meta( $slider->ID, '_startup_reloaded_slider_title_animation', true );
+                    $title_delay         = get_post_meta( $slider->ID, '_startup_reloaded_slider_title_delay', true );
+                    $content_animation   = get_post_meta( $slider->ID, '_startup_reloaded_slider_content_animation', true );
+                    $content_delay       = get_post_meta( $slider->ID, '_startup_reloaded_slider_content_delay', true );
+                    $button_animation    = get_post_meta( $slider->ID, '_startup_reloaded_slider_button_animation', true );
+                    $button_delay        = get_post_meta( $slider->ID, '_startup_reloaded_slider_button_delay', true );
             ?>
 
                 <div id="slide-<?php echo $key ?>" class="item <?php echo ( $key==0 ) ? 'active' : '' ?>" style="<?php if ( $slider_height ) { echo 'height: ' . $slider_height . 'px;'; } else { echo 'height: 550px;';} if ( $bg_image_url ) { echo 'background-image: url('. $bg_image_url .'); background-position: center ' . $background_position . ';';} elseif ( $background_color ) { echo 'background-color: ' . $background_color . ';';} ?>">
