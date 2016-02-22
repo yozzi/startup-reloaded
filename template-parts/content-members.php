b78df6d78cdc2a8885be8f5971387dcebc2cae99
yozz <yozz.online@gmail.com>
2016-02-22 15:22:52 -0500

    dev

--- template-parts/content-members.php
+++ template-parts/content-members.php
@@ -18,14 +18,16 @@ if ( $atts['id'] ) {
 
 
 <img src="<?php echo $member_image_url[0] ?>" class="img-responsive wp-post-image" alt="<?php echo $member->post_title ?>">
-<p class="desc"><?php echo $member->post_content ?></p>
-<ul class="social">
-    <li class="facebook"><a target="_blank" href="<?php echo $member_social_link_1; ?>"><i class="fa fa-<?php echo $member_social_icon_1; ?>"></i></a></li>
-    <li class="twitter"><a target="_blank" href="<?php echo $member_social_link_2; ?>"><i class="fa fa-<?php echo $member_social_icon_2; ?>"></i></a></li>
-    <li class="plusone"><a target="_blank" href="<?php echo $member_social_link_3; ?>"><i class="fa fa-<?php echo $member_social_icon_3; ?>"></i></a></li>
-    <li class="pinterest"><a target="_blank" href="<?php echo $member_social_link_4; ?>"><i class="fa fa-<?php echo $member_social_icon_4; ?>"></i></a></li>
-</ul>
-        
+<p class="desc">
+    <h2><?php echo $member->post_title ?></h2>
+    <?php echo $member->post_content ?>
+    <ul class="social">
+        <li class="facebook"><a target="_blank" href="<?php echo $member_social_link_1; ?>"><i class="fa fa-<?php echo $member_social_icon_1; ?>"></i></a></li>
+        <li class="twitter"><a target="_blank" href="<?php echo $member_social_link_2; ?>"><i class="fa fa-<?php echo $member_social_icon_2; ?>"></i></a></li>
+        <li class="plusone"><a target="_blank" href="<?php echo $member_social_link_3; ?>"><i class="fa fa-<?php echo $member_social_icon_3; ?>"></i></a></li>
+        <li class="pinterest"><a target="_blank" href="<?php echo $member_social_link_4; ?>"><i class="fa fa-<?php echo $member_social_icon_4; ?>"></i></a></li>
+    </ul>
+</p>   
     <?php } else {
     // Si pas d'attribut id ?>
             Vous devez renseigner le ID du menu
