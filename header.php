372a9594194453c406bbf56f50c52e7aef0df607
yozz <yozz.online@gmail.com>
2016-03-11 16:44:25 -0500

    Cleaner html source

--- header.php
+++ header.php
@@ -9,42 +9,42 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <?php
-        //Charger les variables d'Option Framework
-        $logo = of_get_option( 'general-logo' );
-        $responsive = of_get_option( 'general-responsive' );
-        $page_transition = of_get_option( 'page-transition' );
-        $page_transition_in = of_get_option( 'page-transition-in' );
-        $page_transition_out = of_get_option( 'page-transition-out' );
-        $navbar_on = of_get_option( 'navbar-on' );
-        $navbar_position = of_get_option( 'navbar-position' );
-        $navbar_transparent = of_get_option( 'navbar-transparent' );
-        $left_panel_on = of_get_option( 'left-panel-on' );
-        $right_panel_on = of_get_option( 'right-panel-on' );
-        $boxed = of_get_option( 'general-boxed' );
-        
-        if ($logo){$body_logo = 'logo-on';} else {$body_logo = 'logo-off';};
-        if ($navbar_transparent){$body_transparent = 'transparent-on';} else {$body_transparent = 'transparent-off';};
-        if ($navbar_position == 'navbar-static-top'){$body_position = 'static-top';};
-        if ($navbar_position == 'navbar-fixed-top'){$body_position = 'fixed-top';};
-        if ($navbar_position == 'navbar-fixed-slider'){$body_position = 'fixed-slider';};
-        if ($navbar_position == 'navbar-fixed-bottom'){$body_position = 'fixed-bottom';};
-        
-        if ( $responsive ) { //Fonction à compléter mais c'est un bon début ?>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php } ?>
+<?php
+//Charger les variables d'Option Framework
+$logo = of_get_option( 'general-logo' );
+$responsive = of_get_option( 'general-responsive' );
+$page_transition = of_get_option( 'page-transition' );
+$page_transition_in = of_get_option( 'page-transition-in' );
+$page_transition_out = of_get_option( 'page-transition-out' );
+$navbar_on = of_get_option( 'navbar-on' );
+$navbar_position = of_get_option( 'navbar-position' );
+$navbar_transparent = of_get_option( 'navbar-transparent' );
+$left_panel_on = of_get_option( 'left-panel-on' );
+$right_panel_on = of_get_option( 'right-panel-on' );
+$boxed = of_get_option( 'general-boxed' );
 
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+if ($logo){$body_logo = 'logo-on';} else {$body_logo = 'logo-off';};
+if ($navbar_transparent){$body_transparent = 'transparent-on';} else {$body_transparent = 'transparent-off';};
+if ($navbar_position == 'navbar-static-top'){$body_position = 'static-top';};
+if ($navbar_position == 'navbar-fixed-top'){$body_position = 'fixed-top';};
+if ($navbar_position == 'navbar-fixed-slider'){$body_position = 'fixed-slider';};
+if ($navbar_position == 'navbar-fixed-bottom'){$body_position = 'fixed-bottom';};
 
-    <?php wp_head(); ?>
+if ( $responsive ) { //Fonction à compléter mais c'est un bon début ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php } ?>
 
-    <?php get_template_part( 'template-parts/styles', 'header' ) ?>    
-    
-    <?php get_template_part( 'template-parts/scripts', 'header' ) ?>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+
+<?php get_template_part( 'template-parts/styles', 'header' ) ?>    
+
+<?php get_template_part( 'template-parts/scripts', 'header' ) ?>
     
 </head>
 <?php if ( $responsive != 1 ) { $unresponsive = 'ur'; } else { $unresponsive = ''; } ?>
