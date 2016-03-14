b8096b637c16264a9e963e52b3bb4f10999b0924
yozz <yozz.online@gmail.com>
2016-03-14 16:49:58 -0400

    Fix

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="navbar-form form" action="http://192.168.30.101/dev/">
+<form role="search" method="get" class="navbar-form form" action="<?php bloginfo('url'); ?>">
     <div class="form-group">
         <div class="input-group">
             <input class="form-control" placeholder="<?php _e( 'Search...', 'startup-reloaded' ) ?>" value="" name="s" title="<?php _e( 'Search...', 'startup-reloaded' ) ?>" type="search">
