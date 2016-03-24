5172059220b9fd38d5b517aae15de836dfcc841b
yozz <yozz.online@gmail.com>
2016-03-24 10:42:05 -0400

    1 by 1!

--- template-parts/content-partners.php
+++ template-parts/content-partners.php
@@ -11,30 +11,29 @@ $total_partners = count($partners);
     <?php if (is_front_page()) { ?><div class="container"><?php } ?>
         <div class="row">            
             <?php if ($total_partners > 4){ ?>
-                <div id="partners-carousel" class="carousel slide" data-interval="4000" data-ride="carousel">
+                <div id="partners-carousel" class="carousel slide" data-interval="2000" data-ride="carousel" data-type="multi">
                     <div class="carousel-inner">
-                        <?php }  $count = 1; ?>
-                            <div class="item<?php if ($count == 1){ echo ' active';} ?>">
+                        <?php } $count = 1; ?>
+                            
                                 <?php foreach ($partners as $key=> $partner) {
-                                    $logo  = wp_get_attachment_image( get_post_meta( $partner->ID, '_startup_cpt_partners_logo_id', 1 ), 'partners' );
-                                    $url  = get_post_meta( $partner->ID, '_startup_cpt_partners_url', true ); ?>
-                                    <div class="col-sm-3 col-xs-6">
-                                        <div class="partner">
-                                            <div class="partner-inner">
-                                                <?php if ($logo) { ?>
-                                                    <?php if ($url) { ?><a href="<?php echo $url ?>" target="_blank"><?php } ?><?php echo $logo ?><?php if ($url) { ?></a><?php } ?>
-                                                <?php } else { ?>
-                                                <h4><?php if ($url) { ?><a href="<?php echo $url ?>" target="_blank"><?php } ?><?php echo $partner->post_title ?><?php if ($url) { ?></a><?php } ?></h4>
-                                                <?php } ?>
+                                    $logo = wp_get_attachment_image( get_post_meta( $partner->ID, '_startup_cpt_partners_logo_id', 1 ), 'partners' );
+                                    $url = get_post_meta( $partner->ID, '_startup_cpt_partners_url', true ); ?>
+                                    <div class="item<?php if ($count == 1){ echo ' active';} ?>">
+                                        <div class="col-md-3 col-sm-6 col-xs-12">
+                                            <div class="partner">
+                                                <div class="partner-inner">
+                                                    <?php if ($logo) { ?>
+                                                        <?php if ($url) { ?><a href="<?php echo $url ?>" target="_blank"><?php } ?><?php echo $logo ?><?php if ($url) { ?></a><?php } ?>
+                                                    <?php } else { ?>
+                                                    <h4><?php if ($url) { ?><a href="<?php echo $url ?>" target="_blank"><?php } ?><?php echo $partner->post_title ?><?php if ($url) { ?></a><?php } ?></h4>
+                                                    <?php } ?>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <?php if (($count % 4) == 0 && ($count < $total_partners )){ ?>
-                                        </div><div class="item">
-                                    <?php } ?>
-                                
+                            
                                 <?php $count++; } ?>
-                            </div>
+
                         <?php if ($total_partners > 4){ ?>
                     </div>
                 </div>
