c2aa959adb4b8714a4adef23d3477ada8b8d11e8
yozz <yozz.online@gmail.com>
2016-06-15 16:50:04 -0400

    suite

--- theme-my-login/profile-form.php
+++ theme-my-login/profile-form.php
@@ -14,75 +14,77 @@ require get_template_directory() . '/inc/theme-options.php';
                 <?php $template->the_action_template_message( 'profile' ); ?>
                 <?php $template->the_errors(); ?>
                 <form id="your-profile" action="<?php $template->the_action_url( 'profile' ); ?>" method="post">
-                    <?php wp_nonce_field( 'update-user_' . $current_user->ID ); ?>
-                        <input type="hidden" name="from" value="profile" />
-                        <input type="hidden" name="checkuser_id" value="<?php echo $current_user->ID; ?>" />
+                        <?php wp_nonce_field( 'update-user_' . $current_user->ID ); ?>
+                            <input type="hidden" name="from" value="profile" />
+                            <input type="hidden" name="checkuser_id" value="<?php echo $current_user->ID; ?>" />
+                    <div class="form-group">
                         <label for="user_login"><?php _e( 'Username', 'theme-my-login' ); ?></label>
-                        <input type="text" name="user_login" id="user_login" value="<?php echo esc_attr( $profileuser->user_login ); ?>" disabled="disabled" class="regular-text" /> <span class="description"><?php _e( 'Your username cannot be changed.', 'theme-my-login' ); ?></span>
-                        <label for="first_name"><?php _e( 'First Name', 'theme-my-login' ); ?></label>
-                        <input type="text" name="first_name" id="first_name" value="<?php echo esc_attr( $profileuser->first_name ); ?>" class="regular-text" /><label for="last_name"><?php _e( 'Last Name', 'theme-my-login' ); ?></label>
-                        <input type="text" name="last_name" id="last_name" value="<?php echo esc_attr( $profileuser->last_name ); ?>" class="regular-text" />
-                        <label for="nickname"><?php _e( 'Nickname', 'theme-my-login' ); ?> <span class="description"><?php _e( '(required)', 'theme-my-login' ); ?></span></label>
-                        <input type="text" name="nickname" id="nickname" value="<?php echo esc_attr( $profileuser->nickname ); ?>" class="regular-text" />
-                        <label for="display_name"><?php _e( 'Display name publicly as', 'theme-my-login' ); ?></label>
-                        <select name="display_name" id="display_name">
-                            <?php
-                                $public_display = array();
-                                $public_display['display_nickname']  = $profileuser->nickname;
-                                $public_display['display_username']  = $profileuser->user_login;
+                        <input type="text" class="form-control" name="user_login" id="user_login" value="<?php echo esc_attr( $profileuser->user_login ); ?>" disabled="disabled" class="regular-text" /> <span class="description"><?php _e( 'Your username cannot be changed.', 'theme-my-login' ); ?></span>
+                    </div>
+                            <label for="first_name"><?php _e( 'First Name', 'theme-my-login' ); ?></label>
+                            <input type="text" name="first_name" id="first_name" value="<?php echo esc_attr( $profileuser->first_name ); ?>" class="regular-text" /><label for="last_name"><?php _e( 'Last Name', 'theme-my-login' ); ?></label>
+                            <input type="text" name="last_name" id="last_name" value="<?php echo esc_attr( $profileuser->last_name ); ?>" class="regular-text" />
+                            <label for="nickname"><?php _e( 'Nickname', 'theme-my-login' ); ?> <span class="description"><?php _e( '(required)', 'theme-my-login' ); ?></span></label>
+                            <input type="text" name="nickname" id="nickname" value="<?php echo esc_attr( $profileuser->nickname ); ?>" class="regular-text" />
+                            <label for="display_name"><?php _e( 'Display name publicly as', 'theme-my-login' ); ?></label>
+                            <select name="display_name" id="display_name">
+                                <?php
+                                    $public_display = array();
+                                    $public_display['display_nickname']  = $profileuser->nickname;
+                                    $public_display['display_username']  = $profileuser->user_login;
 
-                                if ( ! empty( $profileuser->first_name ) )
-                                    $public_display['display_firstname'] = $profileuser->first_name;
+                                    if ( ! empty( $profileuser->first_name ) )
+                                        $public_display['display_firstname'] = $profileuser->first_name;
 
-                                if ( ! empty( $profileuser->last_name ) )
-                                    $public_display['display_lastname'] = $profileuser->last_name;
+                                    if ( ! empty( $profileuser->last_name ) )
+                                        $public_display['display_lastname'] = $profileuser->last_name;
 
-                                if ( ! empty( $profileuser->first_name ) && ! empty( $profileuser->last_name ) ) {
-                                    $public_display['display_firstlast'] = $profileuser->first_name . ' ' . $profileuser->last_name;
-                                    $public_display['display_lastfirst'] = $profileuser->last_name . ' ' . $profileuser->first_name;
-                                }
+                                    if ( ! empty( $profileuser->first_name ) && ! empty( $profileuser->last_name ) ) {
+                                        $public_display['display_firstlast'] = $profileuser->first_name . ' ' . $profileuser->last_name;
+                                        $public_display['display_lastfirst'] = $profileuser->last_name . ' ' . $profileuser->first_name;
+                                    }
 
-                                if ( ! in_array( $profileuser->display_name, $public_display ) )// Only add this if it isn't duplicated elsewhere
-                                    $public_display = array( 'display_displayname' => $profileuser->display_name ) + $public_display;
+                                    if ( ! in_array( $profileuser->display_name, $public_display ) )// Only add this if it isn't duplicated elsewhere
+                                        $public_display = array( 'display_displayname' => $profileuser->display_name ) + $public_display;
 
-                                $public_display = array_map( 'trim', $public_display );
-                                $public_display = array_unique( $public_display );
+                                    $public_display = array_map( 'trim', $public_display );
+                                    $public_display = array_unique( $public_display );
 
-                                foreach ( $public_display as $id => $item ) {
-                            ?>
-                                <option <?php selected( $profileuser->display_name, $item ); ?>><?php echo $item; ?></option>
-                            <?php
-                                }
-                            ?>
-                        </select>
-                    <label for="email"><?php _e( 'E-mail', 'theme-my-login' ); ?> <span class="description"><?php _e( '(required)', 'theme-my-login' ); ?></span></label>
-                    <input type="text" name="email" id="email" value="<?php echo esc_attr( $profileuser->user_email ); ?>" class="regular-text" />
+                                    foreach ( $public_display as $id => $item ) {
+                                ?>
+                                    <option <?php selected( $profileuser->display_name, $item ); ?>><?php echo $item; ?></option>
+                                <?php
+                                    }
+                                ?>
+                            </select>
+                        <label for="email"><?php _e( 'E-mail', 'theme-my-login' ); ?> <span class="description"><?php _e( '(required)', 'theme-my-login' ); ?></span></label>
+                        <input type="text" name="email" id="email" value="<?php echo esc_attr( $profileuser->user_email ); ?>" class="regular-text" />
 
-                    <?php
-                        foreach ( wp_get_user_contact_methods() as $name => $desc ) {
-                    ?>
-                        <label for="<?php echo $name; ?>"><?php echo apply_filters( 'user_'.$name.'_label', $desc ); ?></label>
-                        <input type="text" name="<?php echo $name; ?>" id="<?php echo $name; ?>" value="<?php echo esc_attr( $profileuser->$name ); ?>" class="regular-text" />
-                    <?php
-                        }
-                    ?>
+                        <?php
+                            foreach ( wp_get_user_contact_methods() as $name => $desc ) {
+                        ?>
+                            <label for="<?php echo $name; ?>"><?php echo apply_filters( 'user_'.$name.'_label', $desc ); ?></label>
+                            <input type="text" name="<?php echo $name; ?>" id="<?php echo $name; ?>" value="<?php echo esc_attr( $profileuser->$name ); ?>" class="regular-text" />
+                        <?php
+                            }
+                        ?>
 
-                    <?php
-                    $show_password_fields = apply_filters( 'show_password_fields', true, $profileuser );
-                    if ( $show_password_fields ) :
-                    ?>
-                    <label for="pass1"><?php _e( 'New Password', 'theme-my-login' ); ?></label>
-                    <input type="password" name="pass1" id="pass1" size="16" value="" autocomplete="off" /> <span class="description"><?php _e( 'If you would like to change the password type a new one. Otherwise leave this blank.', 'theme-my-login' ); ?></span>
-                    <input type="password" name="pass2" id="pass2" size="16" value="" autocomplete="off" /> <span class="description"><?php _e( 'Type your new password again.', 'theme-my-login' ); ?></span>
-                    <div id="pass-strength-result"><?php _e( 'Strength indicator', 'theme-my-login' ); ?></div>
-                    <p class="description indicator-hint"><?php _e( 'Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).', 'theme-my-login' ); ?></p>
-                    <?php endif; ?>
+                        <?php
+                        $show_password_fields = apply_filters( 'show_password_fields', true, $profileuser );
+                        if ( $show_password_fields ) :
+                        ?>
+                        <label for="pass1"><?php _e( 'New Password', 'theme-my-login' ); ?></label>
+                        <input type="password" name="pass1" id="pass1" size="16" value="" autocomplete="off" /> <span class="description"><?php _e( 'If you would like to change the password type a new one. Otherwise leave this blank.', 'theme-my-login' ); ?></span>
+                        <input type="password" name="pass2" id="pass2" size="16" value="" autocomplete="off" /> <span class="description"><?php _e( 'Type your new password again.', 'theme-my-login' ); ?></span>
+                        <div id="pass-strength-result"><?php _e( 'Strength indicator', 'theme-my-login' ); ?></div>
+                        <p class="description indicator-hint"><?php _e( 'Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).', 'theme-my-login' ); ?></p>
+                        <?php endif; ?>
 
-                    <?php do_action( 'show_user_profile', $profileuser ); ?>
-                        <input type="hidden" name="action" value="profile" />
-                        <input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
-                        <input type="hidden" name="user_id" id="user_id" value="<?php echo esc_attr( $current_user->ID ); ?>" />
-                        <input type="submit" class="button-primary" value="<?php esc_attr_e( 'Update Profile', 'theme-my-login' ); ?>" name="submit" />
+                        <?php do_action( 'show_user_profile', $profileuser ); ?>
+                            <input type="hidden" name="action" value="profile" />
+                            <input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
+                            <input type="hidden" name="user_id" id="user_id" value="<?php echo esc_attr( $current_user->ID ); ?>" />
+                            <input type="submit" class="button-primary" value="<?php esc_attr_e( 'Update Profile', 'theme-my-login' ); ?>" name="submit" />
                 </form>
             </div>
         </div>
