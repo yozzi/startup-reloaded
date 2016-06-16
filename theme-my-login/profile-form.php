690fb80aa1352b6a3e1ba0181aeb6f4209f7d433
yozz <yozz.online@gmail.com>
2016-06-16 15:10:57 -0400

    dev

--- theme-my-login/profile-form.php
+++ theme-my-login/profile-form.php
@@ -78,80 +78,70 @@ require get_template_directory() . '/inc/theme-options.php';
                         <input type="text" name="email" id="email" value="<?php echo esc_attr( $profileuser->user_email ); ?>" class="form-control" />
                     </div>
                     
-<?php
-$new_email = get_option( $current_user->ID . '_new_email' );
-if ( $new_email && $new_email['newemail'] != $current_user->user_email ) : ?>
-    <div class="updated inline">
-        <p><?php
-            printf(__( 'There is a pending change of your e-mail to %1$s. <a href="%2$s">Cancel</a>', 'theme-my-login' ), '<code>' . $new_email['newemail'] . '</code>', esc_url( self_admin_url( 'profile.php?dismiss=' . $current_user->ID . '_new_email' ) )
-        ); ?></p>
-    </div>
-<?php endif; ?>
+                    <?php
+                    $new_email = get_option( $current_user->ID . '_new_email' );
+                    if ( $new_email && $new_email['newemail'] != $current_user->user_email ) : ?>
+                        <div class="updated inline">
+                            <p><?php
+                                printf(__( 'There is a pending change of your e-mail to %1$s. <a href="%2$s">Cancel</a>', 'theme-my-login' ), '<code>' . $new_email['newemail'] . '</code>', esc_url( self_admin_url( 'profile.php?dismiss=' . $current_user->ID . '_new_email' ) )
+                            ); ?></p>
+                        </div>
+                    <?php endif; ?>
 
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-<?php
-$show_password_fields = apply_filters( 'show_password_fields', true, $profileuser );
-if ( $show_password_fields ) :
-?>
+                    <?php
+                    $show_password_fields = apply_filters( 'show_password_fields', true, $profileuser );
+                    if ( $show_password_fields ) :
+                    ?>
 
-<table class="tml-form-table">
-<tr id="password" class="user-pass1-wrap">
-<th><label for="pass1"><?php _e( 'New Password', 'theme-my-login' ); ?></label></th>
-<td>
-<input class="hidden" value=" " /><!-- #24364 workaround -->
-<button type="button" class="button button-secondary wp-generate-pw hide-if-no-js"><?php _e( 'Generate Password', 'theme-my-login' ); ?></button>
-<div class="wp-pwd hide-if-js">
-<span class="password-input-wrapper">
-<input type="password" name="pass1" id="pass1" class="regular-text" value="" autocomplete="off" data-pw="<?php echo esc_attr( wp_generate_password( 24 ) ); ?>" aria-describedby="pass-strength-result" />
-</span>
-<div style="display:none" id="pass-strength-result" aria-live="polite"></div>
-<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Hide password', 'theme-my-login' ); ?>">
-<span class="dashicons dashicons-hidden"></span>
-<span class="text"><?php _e( 'Hide', 'theme-my-login' ); ?></span>
-</button>
-<button type="button" class="button button-secondary wp-cancel-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Cancel password change', 'theme-my-login' ); ?>">
-<span class="text"><?php _e( 'Cancel', 'theme-my-login' ); ?></span>
-</button>
-</div>
-</td>
-</tr>
-<tr class="user-pass2-wrap hide-if-js">
-<th scope="row"><label for="pass2"><?php _e( 'Repeat New Password', 'theme-my-login' ); ?></label></th>
-<td>
-<input name="pass2" type="password" id="pass2" class="regular-text" value="" autocomplete="off" />
-<p class="description"><?php _e( 'Type your new password again.', 'theme-my-login' ); ?></p>
-</td>
-</tr>
-<tr class="pw-weak">
-<th><?php _e( 'Confirm Password', 'theme-my-login' ); ?></th>
-<td>
-<label>
-<input type="checkbox" name="pw_weak" class="pw-checkbox" />
-<?php _e( 'Confirm use of weak password', 'theme-my-login' ); ?>
-</label>
-</td>
-</tr>
-<?php endif; ?>
+                        <table class="tml-form-table">
+                            <tr id="password" class="user-pass1-wrap">
+                                <td>
+                                    <input class="hidden" value=" " /><!-- #24364 workaround -->
+                                    <button type="button" class="btn btn-default button button-secondary wp-generate-pw hide-if-no-js"><?php _e( 'Generate Password', 'theme-my-login' ); ?></button>
+                                    <div class="wp-pwd hide-if-js">
+                                        <span class="password-input-wrapper">
+                                            <input type="password" name="pass1" id="pass1" class="regular-text" value="" autocomplete="off" data-pw="<?php echo esc_attr( wp_generate_password( 24 ) ); ?>" aria-describedby="pass-strength-result" />
+                                        </span>
+                                        <div style="display:none" id="pass-strength-result" aria-live="polite"></div>
+                                        <button type="button" class="btn btn-default button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Hide password', 'theme-my-login' ); ?>">
+                                            <span class="dashicons dashicons-hidden"></span>
+                                            <span class="text"><?php _e( 'Hide', 'theme-my-login' ); ?></span>
+                                        </button>
+                                        <button type="button" class="btn btn-default button button-secondary wp-cancel-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Cancel password change', 'theme-my-login' ); ?>">
+                                            <span class="text"><?php _e( 'Cancel', 'theme-my-login' ); ?></span>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr class="user-pass2-wrap hide-if-js">
+                                <td>
+                                    <input name="pass2" type="password" id="pass2" class="regular-text" value="" autocomplete="off" />
+                                    <p class="description"><?php _e( 'Type your new password again.', 'theme-my-login' ); ?></p>
+                                </td>
+                            </tr>
+                            <tr class="pw-weak">
+                                <td>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="pw_weak" class="pw-checkbox" /> <?php _e( 'Confirm use of weak password', 'theme-my-login' ); ?>
+                                        </label>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
 
-</table>
+                    <?php endif; ?>
 
-<?php do_action( 'show_user_profile', $profileuser ); ?>
+                    <?php do_action( 'show_user_profile', $profileuser ); ?>
 
-<input type="hidden" name="action" value="profile" />
-<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
-<input type="hidden" name="user_id" id="user_id" value="<?php echo esc_attr( $current_user->ID ); ?>" />
+                    <input type="hidden" name="action" value="profile" />
+                    <input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
+                    <input type="hidden" name="user_id" id="user_id" value="<?php echo esc_attr( $current_user->ID ); ?>" />
 
-<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Update Profile', 'theme-my-login' ); ?>" name="submit" id="submit" />
+                    <input type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Update Profile', 'theme-my-login' ); ?>" name="submit" id="submit" />
 
-</form>
+                </form>
+            </div>
+        </div>
+    <?php if ( $boxed ){ ?></div><?php } ?>
 </div>
