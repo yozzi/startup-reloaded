6240331384033b6efb7585fe8661659b16a91a3e
yozz <yozz.online@gmail.com>
2016-06-21 16:29:11 -0400

    carousel ou pas

--- template-parts/content-partners.php
+++ template-parts/content-partners.php
@@ -8,8 +8,8 @@ $partners = get_posts( $args );
 $total_partners = count($partners);
 ?>
 <section id="partners"<?php if ( $atts['bg'] ) { ?> style="background:<?php echo $atts['bg'] ?>"<?php } ?>>
-    <?php if (is_front_page()) { ?><div class="container"><?php } ?>
-        <div class="row">            
+    <?php if ( $atts['carousel'] ) { ?>
+        <div class="row">          
             <?php if ($total_partners > 4){ ?>
                 <div id="partners-carousel" class="carousel slide" data-interval="2000" data-ride="carousel" data-type="multi">
                     <div class="carousel-inner partners-inner">
@@ -39,5 +39,23 @@ $total_partners = count($partners);
                 </div>
             <?php } ?>
         </div>
-    <?php if (is_front_page()) { ?></div><?php } ?>
+    <?php } else { ?>
+        <div class="row">
+            <?php foreach ($partners as $key=> $partner) {
+                $logo = wp_get_attachment_image( get_post_meta( $partner->ID, '_startup_cpt_partners_logo_id', 1 ), 'partners' );
+                $url = get_post_meta( $partner->ID, '_startup_cpt_partners_url', true ); ?>
+                    <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
+                        <div class="partner">
+                            <div class="partner-inner">
+                                <?php if ($logo) { ?>
+                                    <?php if ($url) { ?><a href="<?php echo $url ?>" target="_blank"><?php } ?><?php echo $logo ?><?php if ($url) { ?></a><?php } ?>
+                                <?php } else { ?>
+                                <h4><?php if ($url) { ?><a href="<?php echo $url ?>" target="_blank"><?php } ?><?php echo $partner->post_title ?><?php if ($url) { ?></a><?php } ?></h4>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+            <?php } ?> 
+        </div>
+    <?php } ?>
 </section>
